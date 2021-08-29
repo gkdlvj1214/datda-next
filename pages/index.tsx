d3c2c97ad38f1865aa75d2989db21f4fcf04d75d
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-08-29 21:28:55 +0900

    first commit-next setting

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+// index.tsx
+import { FC } from "react";
+
+import { Box } from "@material-ui/core";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <Box>안녕</Box>
+    </div>
+  );
+};
+
+export default Home;
