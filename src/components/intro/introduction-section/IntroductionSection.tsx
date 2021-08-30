5153d85317e51e9c7195d6991eb56b7b78a0fb79
이효신 <leehyosin@ihyosin-ui-MacBook.local>
2021-08-30 23:34:06 +0900

    feat - 인트로 소개글

--- src/components/intro/introduction-section/IntroductionSection.tsx
+++ src/components/intro/introduction-section/IntroductionSection.tsx
@@ -6,25 +6,32 @@ export default function IntroductionSection() {
 
   return (
     <Box display="flex" className={classes.root}>
-      <Container maxWidth="lg">
+      <Container maxWidth="lg" className={classes.titleBox}>
         <Box textAlign="center">
           <Typography variant="h3">우리 아이 유치원 생활</Typography>
           <Typography variant="h3">더욱 스마트하게 관리하기</Typography>
         </Box>
-        <Box>
-          <Button variant="outlined" color="primary">
-            로그인
-          </Button>
-          <Button variant="outlined" color="primary">
-            회원가입
-          </Button>
+        <Box className={classes.buttonBox}>
+          <Button variant="contained">로그인</Button>
+          <Button variant="contained">회원가입</Button>
         </Box>
-        <Box>
+        <Box className={classes.guestLoginBox}>
           <Typography variant="h5">Guest 로그인</Typography>
           <Typography variant="h6">
             아래 버튼은 자동로그인 버튼입니다. 미리 구축된 Datda를 기반으로
-            사이트를 두럴볼 수 있습니다.
+            사이트를 둘러볼 수 있습니다.
           </Typography>
+          <Box>
+            <Button variant="contained" color="primary">
+              기관장 로그인
+            </Button>
+            <Button variant="contained" color="primary">
+              선생님 로그인
+            </Button>
+            <Button variant="contained" color="primary">
+              학부모 로그인
+            </Button>
+          </Box>
         </Box>
       </Container>
     </Box>
--- src/components/intro/introduction-section/style.ts
+++ src/components/intro/introduction-section/style.ts
@@ -1,10 +1,31 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
-const styles = makeStyles({
-  root: {
-    height: "100vh",
-    border: "1px solid",
-  },
-});
+const styles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      height: "100vh",
+      border: "1px solid",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+    titleBox: {},
+    buttonBox: {
+      display: "flex",
+      justifyContent: "center",
+      marginTop: theme.spacing(6),
+      "& button:first-child": {
+        marginRight: theme.spacing(3),
+      },
+    },
+    guestLoginBox: {
+      textAlign: "center",
+      marginTop: theme.spacing(3),
+      background: "rgba(255,255,255,0.35)",
+      borderRadius: 20,
+      border: "1px solid",
+    },
+  })
+);
 
 export default styles;
